123b681e11a74c095b6e94ef42dd8f1e5c65a406
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2021-05-23 16:33:29 +0200

    Use generics.

--- server/views.py
+++ server/views.py
@@ -1,37 +1,20 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from server.models import Sensor
 from server.serializers import SensorSerializer
 
 
-class SensorList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SensorList(generics.ListCreateAPIView):
     """
     List all sensor data, or create a new sensor.
     """
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SensorDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SensorDetails(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a sensor data.
     """
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
